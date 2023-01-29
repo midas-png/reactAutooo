53ebf2ce6b36f4fc9092e04c0ff4ae7925f1fee3
Yan <y4nlvn@gmail.com>
2023-01-29 22:13:48 +0300

    Updates

---
+++ precache-manifest.beda1c0b85dc44b84397ee5f09e0470c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0671c2ae2a7dd9e2a703cb5d9e50d8bb",
+    "url": "/reactAutooo/index.html"
+  },
+  {
+    "revision": "59f88f1d2552076b5e95",
+    "url": "/reactAutooo/static/css/main.7989ae36.chunk.css"
+  },
+  {
+    "revision": "93797fc7755f65fb8117",
+    "url": "/reactAutooo/static/js/2.f0d66507.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/reactAutooo/static/js/2.f0d66507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59f88f1d2552076b5e95",
+    "url": "/reactAutooo/static/js/main.f43d4ee0.chunk.js"
+  },
+  {
+    "revision": "2592c05446ea40dde5e7",
+    "url": "/reactAutooo/static/js/runtime-main.0517ccbc.js"
+  }
+]);
